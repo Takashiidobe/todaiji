55c2951bcbc46a1c5f80c62dc8691141bf10321a
Takashiidobe <idobetakashi@gmail.com>
2025-11-29 12:40:59 -0500

    condense to one example

--- examples/hello_world.asm
+++ examples/hello_world.asm
@@ -4,12 +4,20 @@
 # r2 = buffer pointer (absolute byte offset)
 # r3 = length
 
-        mov.l %r0, $1       # syscall write
-        mov.l %r1, $1       # stdout
-        mov.l %r2, msg      # r2 = &msg
-        mov.l %r3, $12      # length
-        trap                # perform syscall
+        mov.l %r0, $1        # syscall write
+        mov.l %r1, $1        # stdout
+        mov.l %r2, msg       # r2 = &msg
+        mov.l %r3, $12       # length
+        trap                 # perform syscall
+        mov.l %r2, msg_asciz # r2 = &msg
+        trap                 # perform syscall
+        mov.l %r2, msg_ascii # r2 = &msg
+        trap                 # perform syscall
         ret
 
 msg:
         .byte 104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100, 10
+msg_ascii:
+        .ascii "hello ascii\n"
+msg_asciz:
+        .ascii "hello asciz\n"
